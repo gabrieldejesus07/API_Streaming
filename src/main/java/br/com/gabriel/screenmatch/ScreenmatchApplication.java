8c0899329bf6f0eea355d07e8060515c4da1ac3b
gabrieldejesus07 <contatogabrieldejesusaraujo@gmail.com>
2024-04-24 22:05:22 -0300

    first commit

---
+++ src/main/java/br/com/gabriel/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,18 @@
+package br.com.gabriel.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@java.lang.Override
+	public void run(java.lang.String... args) throws Exception {
+		System.out.println("Primeiro projeto spring sem web");
+	}
+}
