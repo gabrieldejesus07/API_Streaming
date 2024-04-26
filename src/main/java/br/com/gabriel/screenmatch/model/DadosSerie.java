de0bc0d033c9f181d15afdda3c60ac7408458068
Gabriel de Jesus <contatogabrieldejesusaraujo@gmail.com>
2024-04-26 17:05:48 -0300

    third commit

---
+++ src/main/java/br/com/gabriel/screenmatch/model/DadosSerie.java
@@ -0,0 +1,8 @@
+package br.com.gabriel.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
