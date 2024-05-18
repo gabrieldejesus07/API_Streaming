0fa7c290e67d3475590ae0f71aa9608534641724
Gabriel de Jesus <contatogabrieldejesusaraujo@gmail.com>
2024-05-18 11:07:20 -0300

    ninth commit

--- src/main/java/br/com/gabriel/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/gabriel/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.gabriel.screenmatch;
 
-import br.com.gabriel.screenmatch.model.DadosEpisodio;
-import br.com.gabriel.screenmatch.model.DadosSerie;
-import br.com.gabriel.screenmatch.model.DadosTemporada;
-import br.com.gabriel.screenmatch.service.ConsumoApi;
-import br.com.gabriel.screenmatch.service.ConverteDados;
+import br.com.gabriel.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,23 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@java.lang.Override
 	public void run(java.lang.String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDador("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDador("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 0; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDador("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/gabriel/screenmatch/principal/Principal.java
+++ src/main/java/br/com/gabriel/screenmatch/principal/Principal.java
@@ -1,7 +1,12 @@
 package br.com.gabriel.screenmatch.principal;
 
+import br.com.gabriel.screenmatch.model.DadosSerie;
 import br.com.gabriel.screenmatch.service.ConsumoApi;
+import br.com.gabriel.screenmatch.service.ConverteDados;
+import br.com.gabriel.screenmatch.model.DadosTemporada;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -11,13 +16,26 @@ public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu(){
         System.out.println("Digite o nome da série para busca:");
         var nomeSerie = leitura.nextLine();
-        var json = consumo.obterDador(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        var json = consumo.obterDador(ENDERECO + nomeSerie.replace(" ", "+")  + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 0; i <= dados.totalTemporadas(); i++) {
+			json = consumo.obterDador(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 
 
-        //"https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c"
+
+
     }
 }
